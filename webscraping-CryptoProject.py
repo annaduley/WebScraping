96a7679bd92ad803754b634add1f1f357e1e8632
Anna <anna_dulevitz1@baylor.edu>
2022-11-15 11:08:07 -0600

    rearranged code

--- webscraping-CryptoProject.py
+++ webscraping-CryptoProject.py
@@ -19,6 +19,26 @@ soup = BeautifulSoup(webpage, 'html.parser')
 tr = soup.findAll('tr')
 
 
+#tests if Bitcoin is below 40,000
+for row in tr:
+    td = row.findAll("td")
+    if td:
+      if ((td[1].text).split()[0]) == 'Bitcoin' and((td[1].text).split()[1]) == 'BTC':
+        num = float(((td[2].text.strip(' ')).strip('$')).replace(',',''))
+        if((float(((td[2].text.strip(' ')).strip('$')).replace(',','')))<40000):
+          textmsg = client.messages.create(to=myCellPhone, from_=TwilioNumber, body = "Bitcoin has fallen below $40,000")
+
+
+#tests if Ethereum is below 3,000
+for row in tr:
+    td = row.findAll("td")
+    if td:
+      if ((td[1].text).split()[0]) == 'Ethereum' and((td[1].text).split()[1]) == 'ETH':
+        num = float(((td[2].text.strip(' ')).strip('$')).replace(',',''))
+        if((float(((td[2].text.strip(' ')).strip('$')).replace(',','')))<3000):
+          textmsg = client.messages.create(to=myCellPhone, from_=TwilioNumber, body = "Ethereum has fallen below $3,000")
+
+
 #displays output of top 5 currencies
 for row in tr[0:6]:
     td = row.findAll("td")
@@ -38,22 +58,3 @@ for row in tr[0:6]:
 
       
 
-
-#tests if Bitcoin is below 40,000
-for row in tr:
-    td = row.findAll("td")
-    if td:
-      if ((td[1].text).split()[0]) == 'Bitcoin' and((td[1].text).split()[1]) == 'BTC':
-        num = float(((td[2].text.strip(' ')).strip('$')).replace(',',''))
-        if((float(((td[2].text.strip(' ')).strip('$')).replace(',','')))<40000):
-          textmsg = client.messages.create(to=myCellPhone, from_=TwilioNumber, body = "Bitcoin has fallen below $40,000")
-
-
-#tests if Ethereum is below 3,000
-for row in tr:
-    td = row.findAll("td")
-    if td:
-      if ((td[1].text).split()[0]) == 'Ethereum' and((td[1].text).split()[1]) == 'ETH':
-        num = float(((td[2].text.strip(' ')).strip('$')).replace(',',''))
-        if((float(((td[2].text.strip(' ')).strip('$')).replace(',','')))<3000):
-          textmsg = client.messages.create(to=myCellPhone, from_=TwilioNumber, body = "Ethereum has fallen below $3,000")
